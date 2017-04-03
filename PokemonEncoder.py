926fb1cecb4339b665eae3e478486ad284a5d5ce
camkn <camknightt@gmail.com>
2017-04-02 21:07:06 -0400

    adjusted a few things, added sofmax function. it actually sorta works

--- PokemonEncoder.py
+++ PokemonEncoder.py
@@ -48,14 +48,21 @@ def autoencoder():
                'conv2':tf.Variable(tf.random_normal([5,5,32,64])),
                'fc':tf.Variable(tf.random_normal([43*31*64,1024])),
                'out':tf.Variable(tf.random_normal([1024, n_dims])),
-                'layer1': tf.Variable(tf.random_normal([n_dims,x_dim*y_dim*n_filters])),
+         'layer1': tf.Variable(tf.random_normal([n_dims, 1024])),
+         'layer2': tf.Variable(tf.random_normal([1024, 2048])),
+         'layer3': tf.Variable(tf.random_normal([2048, 512])),
+         'layer4': tf.Variable(tf.random_normal([512,x_dim*y_dim*n_filters])),
+
                 'convT': tf.Variable(tf.random_normal([x_dim*2,y_dim*2,n_filters,n_filters*2]))}
 
     b = {'conv1':tf.Variable(tf.random_normal([32])),
                'conv2':tf.Variable(tf.random_normal([64])),
                'fc':tf.Variable(tf.random_normal([1024])),
                'out':tf.Variable(tf.random_normal([n_dims])),
-                'layer1': tf.Variable(tf.random_normal([x_dim*y_dim*n_filters]))}
+                'layer1': tf.Variable(tf.random_normal([1024])),
+         'layer2': tf.Variable(tf.random_normal([2048])),
+         'layer3': tf.Variable(tf.random_normal([512])),
+         'layer4': tf.Variable(tf.random_normal([x_dim * y_dim * n_filters]))}
 
     def conv2d(x, W):
         return tf.nn.conv2d(x, W, strides=[1, 1, 1, 1], padding='SAME')
@@ -77,7 +84,13 @@ def autoencoder():
 
     output = tf.matmul(fc, w['out']) + b['out']
 
-    output_connected = tf.matmul(output, w['layer1']) + b['layer1']
+    l1 = tf.nn.softmax(tf.matmul(output, w['layer1']) + b['layer1'])
+    l2 = tf.nn.softmax(tf.matmul(l1, w['layer2']) + b['layer2'])
+    l3 = tf.nn.softmax(tf.matmul(l2, w['layer3']) + b['layer3'])
+    output_connected = tf.matmul(l3, w['layer4']) + b['layer4']
+
+
+
     reshape_decode = tf.reshape(output_connected, [1,x_dim,y_dim,n_filters])
 
     autoencoded_output = reshape_decode
@@ -90,19 +103,22 @@ def autoencoder():
 
     cost = tf.reduce_sum(tf.square(autoencoded_output - x))
 
-    opt = tf.train.AdamOptimizer(0.01).minimize(cost)
+    opt = tf.train.AdamOptimizer(0.1).minimize(cost)
 
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
         i = 0
         for filename in os.listdir(directory):
-            img = load_image_values(directory+filename)
-            feed_dict = {x: [img]}
-            r_cost, out_img,_ = sess.run([cost,autoencoded_output,opt],feed_dict=feed_dict)
-            print("Epoch " + str(i) + " Cost: " + r_cost)
-            out_img = np.array(out_img).squeeze()
-            if i % 20 == 0:
-                convert_array_to_image(arr=out_img,disp=True)
+            try:
+                img = load_image_values(directory+filename)
+                feed_dict = {x: [img]}
+                r_cost, out_img,_ = sess.run([cost,autoencoded_output,opt],feed_dict=feed_dict)
+                print("Epoch " + str(i) + " Cost: " + str(r_cost))
+                out_img = np.array(out_img).squeeze()
+                if i % 100 == 0:
+                    convert_array_to_image(arr=out_img,disp=True)
+            except:
+                print("Failed: " + filename)
             i += 1
 
 
